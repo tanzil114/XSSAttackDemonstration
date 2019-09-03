e1e3e6b240f9329b9555b0c18a5eb08ab27db746
tanzil114 <tanzilbaghoor@gmail.com>
2019-09-03 17:40:46 +0500

    XSSAttackDemonstration added

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ const port = 3000
 // router.get('/', (req, res) => res.send('index.html'))
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')))
-app.get('/storedAttackStore', (req, res) => res.sendFile(path.join(__dirname + '/storedAttackStore.html')))
-app.get('/storedAttackRetrieve', (req, res) => res.sendFile(path.join(__dirname + '/storedAttackRetrieve.html')))
-app.get('/reflectedAttack', (req, res) => res.sendFile(path.join(__dirname + '/reflectedAttack.html')))
-app.get('/domAttack', (req, res) => res.sendFile(path.join(__dirname + '/domAttack.html')))
+app.get('/storedAttackStore', (req, res) => res.sendFile(path.join(__dirname + 'XSSAttackDemonstration/storedAttackStore.html')))
+app.get('/storedAttackRetrieve', (req, res) => res.sendFile(path.join(__dirname + 'XSSAttackDemonstration/storedAttackRetrieve.html')))
+app.get('/reflectedAttack', (req, res) => res.sendFile(path.join(__dirname + 'XSSAttackDemonstration/reflectedAttack.html')))
+app.get('/domAttack', (req, res) => res.sendFile(path.join(__dirname + 'XSSAttackDemonstration/domAttack.html')))
 app.get('/js', (req, res) => res.sendFile(path.join(__dirname + '/learnjs.html')))
 
 app.use(function (req, res, next) {
